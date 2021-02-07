e3e1d8acf9ac698642d3d2e0d88695015a5e7998
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-07 13:33:59 +0100

    mobiistar_C1_Shine: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: mobiistar Model: mobiistar C1 Shine Script version: 1.2.3 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_mobiistar_C1_Shine.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from mobiistar_C1_Shine device
+$(call inherit-product, device/mobiistar/mobiistar_C1_Shine/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mobiistar_C1_Shine
+PRODUCT_NAME := omni_mobiistar_C1_Shine
+PRODUCT_BRAND := mobiistar
+PRODUCT_MODEL := mobiistar C1 Shine
+PRODUCT_MANUFACTURER := mobiistar
+PRODUCT_RELEASE_NAME := mobiistar mobiistar C1 Shine
